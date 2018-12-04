7d743256d4db0423860fd39c5f65b76ece6b6e04
vbruttin <43440719+vbruttin@users.noreply.github.com>
2018-12-04 13:05:49 +0100

    yes

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from kivy.uix.gridlayout import GridLayout
 
 Builder.load_file('manager.kv')
 Builder.load_file('screenhome.kv')
@@ -27,7 +28,7 @@ class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior,
     ''' Adds selection and focus behaviour to the view. '''
 
 
-class SelectableButton(RecycleDataViewBehavior, Label):
+class SelectableGrid(RecycleDataViewBehavior, GridLayout):
     ''' Add selection support to the Label '''
     index = None
     selected = BooleanProperty(False)
@@ -36,12 +37,15 @@ class SelectableButton(RecycleDataViewBehavior, Label):
     def refresh_view_attrs(self, rv, index, data):
         ''' Catch and handle the view changes '''
         self.index = index
-        return super(SelectableButton, self).refresh_view_attrs(
+        self.ids['id_label1'].text = data['label1']['text']
+        self.ids['id_label2'].text = data['label2']['text']
+        self.ids['id_label3'].text = data['label3']['text']
+        return super(SelectableGrid, self).refresh_view_attrs(
             rv, index, data)
 
     def on_touch_down(self, touch):
         ''' Add selection on touch down '''
-        if super(SelectableButton, self).on_touch_down(touch):
+        if super(SelectableGrid, self).on_touch_down(touch):
             return True
         if self.collide_point(*touch.pos) and self.selectable:
             return self.parent.select_with_touch(self.index, touch)
@@ -50,9 +54,23 @@ class SelectableButton(RecycleDataViewBehavior, Label):
         ''' Respond to the selection of items in the view. '''
         self.selected = is_selected
 
+class SelectableQuantity(RecycleDataViewBehavior, GridLayout):
+    ''' Add selection support to the Label '''
+    index = None
+    selected = BooleanProperty(False)
+    selectable = BooleanProperty(True)
+
+    def refresh_view_attrs(self, rv, index, data):
+        ''' Catch and handle the view changes '''
+        self.index = index
+        self.ids['id_label1'].text = data['label1']['text']
+        self.ids['id_label2'].text = data['label2']['text']
+        return super(SelectableQuantity, self).refresh_view_attrs(
+            rv, index, data)
+
 class RV(RecycleView):
     
-    df = pd.read_csv('../data/data_food_final.csv')
+    df = pd.read_csv('../data/OpenFoodFacts_final.csv', low_memory=False)
     def __init__(self, **kwargs):
         super(RV, self).__init__(**kwargs)
 
@@ -63,47 +81,73 @@ class RV(RecycleView):
         else:
             isinside = self.df['product_name'].str.contains('', case=False)
             for item in query.split():
-                isinside = isinside & self.df['product_name'].str.contains(item, case=False)
+                isinside = isinside & \
+                    (self.df['product_name'].str.contains(item, case=False) | \
+                    self.df['Brands'].str.contains(item, case=False))
             if any(isinside):
                 selection = self.df[isinside]
-                self.data = [{'text': str(row[1]) + '  -  ' +str(row[2]), 'font_size': 20} for row in selection.itertuples()]
+                self.data = []
+                for row in selection.itertuples():
+                    d = {'label1': {'text': str(row[1])}, \
+                        'label2': {'text': str(row[2])},
+                        'label3': {'text': str(row[-1])}}
+                    self.data.append(d)
             else:
-                isinside = self.df['code'].str.match(query, case=False)
+                isinside = self.df['code'].str.contains(query, case=False)
                 if any(isinside):
                     selection = self.df[isinside]
-                    self.data = [{'text': str(row[1]) + '  -  ' +str(row[2]), 'font_size': 20} for row in selection.itertuples()]
+                    for row in selection.itertuples():
+                        d = {'label1': {'text': str(row[1])}, \
+                            'label2': {'text': str(row[2])},
+                            'label3': {'text': str(row[-1])}}
+                        self.data.append(d)                
                 else:
-                    self.data = [{'text' : 'No product found'}]
+                    self.data = [{'label1': {'text': ''}, \
+                        'label2': {'text': 'No product found'}, 'label3': {'text': ''}}]
+    
+    def getQuantities(self, dict):
+        self.data = []
+        code = dict['code']
+        product_name = dict['product_name']
+        for index in range(len(code)):
+            d = {'label1': {'text': code[index]}, 'label2': {'text': product_name[index]}, 'label3': {'text': ''}}
+            self.data.append(d)
 
 class ScreenHome(Screen):
     pass
 
 class ScreenProduct(Screen):
-    temp_dict = {'code':''}
-    def getSelection(self, text, state):
+    temp_dict = {'code':'', 'product_name': ''}
+    def getSelection(self, text1, text2, state):
         if state:
-            self.temp_dict['code'] = text
+            self.temp_dict['code'] = text1
+            self.temp_dict['product_name'] = text2
         else:
             self.temp_dict['code'] = ''
-
-
+            self.temp_dict['product_name'] = ''
 
 class ScreenQuantities(Screen):
-    pass
+    def getSelection(self, text, state):
+        if state:
+            pass    
 
 class Manager(ScreenManager):
-    selected_products = {'product': [], 'quantity': []} 
+    selected_products = {'code': [], 'product_name': [], 'quantity': []} 
 
     def addProduct(self):
-        item = self.ids.screen_product.temp_dict['code']
-        if item != '':
-            self.selected_products['product'].append(item)
+        item1 = self.ids.screen_product.temp_dict['code']
+        item2 = self.ids.screen_product.temp_dict['product_name']
+        if item1 != '' and item2 != '':
+            self.selected_products['code'].append(item1)
+            self.selected_products['product_name'].append(item2)
         print(self.selected_products)
 
     def deleteProduct(self):
-        item = self.ids.screen_product.temp_dict['code']
-        if item in self.selected_products['product']:
-            self.selected_products['product'].remove(item)
+        item1 = self.ids.screen_product.temp_dict['code']
+        item2 = self.ids.screen_product.temp_dict['product_name']
+        if item1 in self.selected_products['code'] and item2 in self.selected_products['product_name']:
+            self.selected_products['code'].remove(item1)
+            self.selected_products['product_name'].remove(item2)
         print(self.selected_products)
 
 class NutriScoreApp(App):
