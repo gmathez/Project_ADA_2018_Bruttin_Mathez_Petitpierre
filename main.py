c240ddb042a65029d2129ded621470b458e18084
vbruttin <43440719+vbruttin@users.noreply.github.com>
2018-12-10 15:00:53 +0100

    profile

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from kivy.uix.recycleboxlayout import RecycleBoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.checkbox import CheckBox
+from kivy.uix.spinner import Spinner
 from kivy.uix.recycleview import RecycleView
 from kivy.uix.recycleview.views import RecycleDataViewBehavior
 from kivy.uix.behaviors import FocusBehavior
@@ -14,13 +15,14 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 
 # Import the kv files
-Builder.load_file('manager.kv')
-Builder.load_file('screenhome.kv')
 Builder.load_file('rv.kv')
+Builder.load_file('screenhome.kv')
+Builder.load_file('screenprofile.kv')
+Builder.load_file('screensettings.kv')
 Builder.load_file('screenproduct.kv')
 Builder.load_file('screenquantities.kv')
-Builder.load_file('screensettings.kv')
 Builder.load_file('screenfinal.kv')
+Builder.load_file('manager.kv')
 
 import pandas as pd
 import re
@@ -130,10 +132,147 @@ class RV(RecycleView):
                 'label3': {'text': quantity[index]}}
             self.data.append(d)
 
-    
 class ScreenHome(Screen):
     # Class for the Home Screen. No variables or functions needed for this screen
     pass
+
+class ScreenProfile(Screen):
+    # Class for the Profile Screen
+    pass
+
+class ScreenSettings(Screen):
+    # Class for the Settings Screen
+    settings = {'rec': True,'name': '', 'surname': '', 'age': 0, 'sex': True, 'weight': 0, \
+            'email': '', 'activity': 0, 'days': 0}
+    id_profile = -999
+
+    def resetForm(self):
+        # Reset the indicators of invalid input
+        self.ids.sex.color = (1,1,1,1)
+        self.ids.activity.color = (1,1,1,1)
+        self.ids.age.hint_text_color = (0.5, 0.5, 0.5, 1.0)
+        self.ids.weight.hint_text_color = (0.5, 0.5, 0.5, 1.0)
+        self.ids.days.hint_text_color = (0.5, 0.5, 0.5, 1.0)
+        self.ids.email.hint_text_color = (0.5, 0.5, 0.5, 1.0)
+        self.ids.name.hint_text_color = (0.5, 0.5, 0.5, 1.0)
+        self.ids.surname.hint_text_color = (0.5, 0.5, 0.5, 1.0)
+
+    def setForm(self, id_profile):
+        self.id_profile = id_profile
+        self.settings = {'rec': True,'name': '', 'surname': '', 'age': 0, 'sex': True, 'weight': 0, \
+            'email': '', 'activity': 0, 'days': 0}
+
+        if int(self.id_profile) >= 0:
+            self.ids.name.text = str(profile_list.iloc[self.id_profile]['name'])
+            self.ids.surname.text= str(profile_list.iloc[self.id_profile]['surname'])
+            self.ids.age.text = str(profile_list.iloc[self.id_profile]['age'])
+            if bool(profile_list.iloc[self.id_profile]['sex']):
+                self.ids.male.active = True
+                self.ids.female.active = False
+
+            else:
+                self.ids.male.active = False
+                self.ids.female.active = True
+
+            self.ids.weight.text = str(profile_list.iloc[self.id_profile]['weight'])
+            self.ids.email.text = str(profile_list.iloc[self.id_profile]['email'])
+            self.ids.days.text = str(profile_list.iloc[self.id_profile]['days'])
+            if int(profile_list.iloc[self.id_profile]['activity']) == 1.8:
+                self.ids.seated.active = False
+                self.ids.both.active = False
+                self.ids.standing.active = True
+
+            elif int(profile_list.iloc[self.id_profile]['activity']) == 1.6:
+                self.ids.seated.active = False
+                self.ids.both.active = True
+                self.ids.standing.active = False
+
+            else:
+                self.ids.seated.active = True
+                self.ids.both.active = False
+                self.ids.standing.active = False
+        else:
+            self.ids.name.text = ''
+            self.ids.surname.text = ''
+            self.ids.age.text = ''
+            self.ids.male.active = False
+            self.ids.female.active = False
+            self.ids.email.text = ''
+            self.ids.weight.text = ''
+            self.ids.seated.active = False
+            self.ids.both.active = False
+            self.ids.standing.active = False
+            self.ids.days.text = ''
+
+    def changeScreen(self, valid):
+        # Handle the validity of the inputs and the change of current screen
+        if valid:
+            self.resetForm()
+            # Check name validity
+            if self.ids.name.text.strip() == '':
+                self.ids.name.hint_text_color = (1,0,0,1)
+                return False
+            # Check surname validity
+            elif self.ids.surname.text.strip() == '':
+                self.ids.surname.hint_text_color = (1,0,0,1)
+                return False
+            # Check age validity
+            elif self.ids.age.text.strip() == '' or int(self.ids.age.text) <= 0 or \
+              int(self.ids.age.text) >= 120:
+                self.ids.age.text = ''
+                self.ids.age.hint_text_color = (1,0,0,1)
+                return False
+            # Check sex validity
+            elif not(self.ids.male.active or self.ids.female.active):
+                self.ids.sex.color = (1,0,0,1)  
+                return False
+            # Check email validity
+            elif not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", self.ids.email.text):
+                self.ids.email.text = ''
+                self.ids.email.hint_text_color = (1,0,0,1)
+                return False
+            # Check weight validity
+            elif self.ids.weight.text.strip() == '' or int(self.ids.weight.text) <= 0:
+                self.ids.weight.text = ''
+                self.ids.weight.hint_text_color = (1,0,0,1)
+                return False 
+            # Check activity validity
+            elif not(self.ids.seated.active or self.ids.both.active or self.ids.standing.active):
+                self.ids.activity.color = (1,0,0,1)
+                return False
+            # Check days validity
+            elif self.ids.days.text.strip() == '' or int(self.ids.days.text) <= 0:
+                self.ids.days.text = ''
+                self.ids.days.hint_text_color = (1,0,0,1)
+                return False
+            
+            else: # Validation of the form and reset
+                self.settings['rec'] = True
+                self.settings['name'] = self.ids.name.text
+                self.settings['surname'] = self.ids.surname.text
+                self.settings['age'] = int(self.ids.age.text)
+                self.settings['weight'] = int(self.ids.weight.text)
+                self.settings['email'] = self.ids.email.text
+                self.settings['days'] = int(self.ids.days.text)
+                self.settings['sex'] = self.ids.male.active
+
+                if self.ids.seated.active:
+                    self.settings['activity'] = 1.4
+
+                if self.ids.both.active:
+                    self.settings['activity'] = 1.6
+
+                if self.ids.standing.active:
+                    self.settings['activity'] = 1.8
+
+                self.resetForm()
+
+        else: # If the user pass the settings screen
+            self.settings['rec'] = False
+
+        self.manager.setSettings(self.settings, self.id_profile)
+        # Change the current screen
+        self.manager.current = 'Product Screen'
 
 class ScreenProduct(Screen):
     # Class for the Product Screen
@@ -164,6 +303,9 @@ class ScreenQuantities(Screen):
         # Store the quantities input by the user
         l = len(self.temp_dict['quantity'])
 
+        if text3 == '' or text3 == '-' or int(text3) < 0:
+            text3 = '0'
+
         if index < l:
             self.temp_dict['code'][index] = text1
             self.temp_dict['product_name'][index] = text2
@@ -175,108 +317,6 @@ class ScreenQuantities(Screen):
             self.temp_dict['product_name'] = self.temp_dict['product_name'] + temp + [text2]
             self.temp_dict['quantity'] = self.temp_dict['quantity'] + temp + [text3]
 
-
-class ScreenSettings(Screen):
-    # Class for the Settings Screen
-    settings = {'rec': True,'name': '', 'surname': '', 'age': 0, 'sex': True, 'weight': 0, \
-            'email': '', 'activity': 0, 'days': 0}
-
-    def resetForm(self):
-        # Reset the indicators of invalid input
-        self.ids.sex.color = (1,1,1,1)
-        self.ids.age.color = (1,1,1,1)
-        self.ids.weight.color = (1,1,1,1)
-        self.ids.activity.color = (1,1,1,1)
-        self.ids.days.color = (1,1,1,1)
-        self.ids.sex.hint_text_color = (0.5, 0.5, 0.5, 1.0)
-        self.ids.age.hint_text_color = (0.5, 0.5, 0.5, 1.0)
-        self.ids.weight.hint_text_color = (0.5, 0.5, 0.5, 1.0)
-        self.ids.activity.hint_text_color = (0.5, 0.5, 0.5, 1.0)
-        self.ids.days.hint_text_color = (0.5, 0.5, 0.5, 1.0)
-        self.ids.email.foreground_color = (1,1,1,1)
-        self.ids.email.hint_text_color = (0.5, 0.5, 0.5, 1.0)
-        self.ids.name.foreground_color = (1,1,1,1)
-        self.ids.name.hint_text_color = (0.5, 0.5, 0.5, 1.0)
-        self.ids.surname.foreground_color = (1,1,1,1)
-        self.ids.surname.hint_text_color = (0.5, 0.5, 0.5, 1.0)
-
-    def changeScreen(self, valid):
-        # Handle the validity of the inputs and the change of current screen
-        if valid:
-            self.resetForm()
-            # Check name validity
-            if self.ids.name.text.strip() == '' and self.ids.name.text != 'Invalid name':
-                self.ids.name.text = 'Invalid name'
-                self.ids.name.foreground_color = (1,0,0,1)
-                self.ids.name.hint_text_color = (1,0,0,1)
-                return False
-            # Check surname validity
-            elif self.ids.surname.text.strip() == '' and self.ids.surname.text != 'Invalid surname':
-                self.ids.surname.text = 'Invalid surname'
-                self.ids.surname.foreground_color = (1,0,0,1)
-                self.ids.surname.hint_text_color = (1,0,0,1)
-                return False
-            # Check age validity
-            elif self.ids.age.text.strip() == '' or int(self.ids.age.text) <= 0 or \
-              int(self.ids.age.text) >= 120:
-                self.ids.age.color = (1,0,0,1)
-                self.ids.age.hint_text_color = (1,0,0,1)
-                return False
-            # Check sex validity
-            elif not(self.ids.male.active or self.ids.female.active):
-                self.ids.sex.color = (1,0,0,1)  
-                self.ids.sex.hint_text_color = (1,0,0,1) 
-                return False
-            # Check email validity
-            elif not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", self.ids.email.text):
-                self.ids.email.text = 'Invalid email'
-                self.ids.email.foreground_color = (1,0,0,1)
-                self.ids.email.hint_text_color = (1,0,0,1)
-                return False
-            # Check weight validity
-            elif self.ids.weight.text.strip() == '' or int(self.ids.weight.text) <= 0:
-                self.ids.weight.color = (1,0,0,1)
-                self.ids.weight.hint_text_color = (1,0,0,1)
-                return False 
-            # Check activity validity
-            elif not(self.ids.seated.active or self.ids.both.active or self.ids.standing.active):
-                self.ids.activity.color = (1,0,0,1)
-                self.ids.activity.hint_text_color = (1,0,0,1)
-                return False
-            # Check days validity
-            elif self.ids.days.text.strip() == '' or int(self.ids.days.text) <= 0:
-                self.ids.days.color = (1,0,0,1)
-                self.ids.days.hint_text_color = (1,0,0,1)
-                return False
-            
-            else: # Validation of the form and reset
-                self.settings['rec'] = True
-                self.settings['name'] = self.ids.name.text
-                self.settings['surname'] = self.ids.surname.text
-                self.settings['age'] = int(self.ids.age.text)
-                self.settings['weight'] = int(self.ids.weight.text)
-                self.settings['email'] = self.ids.email.text
-                self.settings['days'] = int(self.ids.days.text)
-                self.settings['sex'] = self.ids.male.active
-
-                if self.ids.seated.active:
-                    self.settings['activity'] = 1.4
-
-                if self.ids.both.active:
-                    self.settings['activity'] = 1.6
-
-                if self.ids.standing.active:
-                    self.settings['activity'] = 1.8
-
-                self.resetForm()
-
-        else: # If the user pass the settings screen
-            self.settings['rec'] = False
-
-        self.manager.setSettings(self.settings)
-        # Change the current screen
-        self.manager.current = 'Final Screen'
-
 class ScreenFinal(Screen):
     # Class for the Final Screen. No variables or functions needed for this screen
     pass
@@ -286,6 +326,23 @@ class Manager(ScreenManager):
     selected_products = {'code': [], 'product_name': [], 'quantity': []}
     settings = {'Rec': True, 'Name': '', 'Surname': '', 'Email': '', 'Age': 0, 'Sex': True, 'Pal': 0, \
             'Weight': 0, 'Day': 0}
+
+    def getProfiles(self):
+        self.ids.screen_profile.ids.profile_spinner.values = \
+            [str(index + 1) + ' : ' + profile_list['name'][index] + ' ' + profile_list['surname'][index] \
+            for index in profile_list.index]
+
+    def toSettings(self, text):
+        if text != '':
+            items = text.split()
+            id_profile = items[0].strip()
+            id_profile = int(id_profile) - 1
+        else:
+            id_profile = -999
+
+        self.ids.screen_settings.setForm(id_profile)
+        self.current = 'Settings Screen'
+        
 
     def addProduct(self):
         # Add product to main storage
@@ -317,9 +374,9 @@ class Manager(ScreenManager):
             if  self.selected_products['quantity'][item] == '':
                 self.selected_products['quantity'][item] = '0'
             
-        self.current = 'Settings Screen'
+        self.current = 'Final Screen'
 
-    def setSettings(self, data):
+    def setSettings(self, data, new):
         # Add settings to main storage
         self.settings['Rec'] = data['rec']
         self.settings['Name'] = data['name']
@@ -330,6 +387,21 @@ class Manager(ScreenManager):
         self.settings['Day'] = data['days']
         self.settings['Sex'] = data['sex']
         self.settings['Age'] = data['age']
+
+        if new == -999:
+            temp_df = pd.DataFrame.from_dict({'index': [len(profile_list)], 'name': [data['name']], 'surname': [data['surname']], \
+                'age': [data['age']], 'sex': [data['sex']], 'email': [data['email']], 'weight': [data['weight']], \
+                'activity': [data['activity']], 'days': [data['days']]}).set_index('index')
+            new_profile_list = pd.concat([profile_list, temp_df]) 
+        else:
+            temp_df = pd.DataFrame.from_dict({'name': [data['name']], 'surname': [data['surname']], \
+                'age': [data['age']], 'sex': [data['sex']], 'email': [data['email']], 'weight': [data['weight']], \
+                'activity': [data['activity']], 'days': [data['days']]})
+            new_profile_list= profile_list
+            new_profile_list.iloc[new,:] = temp_df
+
+        new_profile_list.to_csv('./data/profile.csv', sep=';')
+
 
     def computation(self):
         # Call algo for computation of NutriScore and recommendation. Display results.
@@ -351,9 +423,16 @@ class NutriScoreApp(App):
     # Main class of the App
     def build(self):
         # Import the database for the whole application
-        global DF, allTrue
-        DF = pd.read_csv('./data/OpenFoodFacts_final.csv', low_memory=False, index_col = [0])
-        allTrue = DF['product_name'].str.contains('', case=False) # True Vector of length len(DF)
+        global DF, allTrue, profile_list
+
+        try:
+            DF = pd.read_csv('../data/OpenFoodFacts_final.csv', low_memory=False, index_col = [0])
+            allTrue = DF['product_name'].str.contains('', case=False) # True Vector of length len(DF)
+            profile_list = pd.read_csv('./data/profile.csv', sep=';', index_col=[0])
+
+        except:
+            print('Fatal error: files missing')  
+                  
         return Manager()
 
 if __name__ == '__main__':
