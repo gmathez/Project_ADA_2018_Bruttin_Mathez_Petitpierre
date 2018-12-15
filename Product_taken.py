1b83984e75f4182216385a945b795b78d01a0618
Rémi Petitpierre <38254995+RPetitpierre@users.noreply.github.com>
2018-12-15 09:45:03 +0100

    Commenting

--- Product_taken.py
+++ Product_taken.py
@@ -1,7 +1,19 @@
+# Import
 import pandas as pd 
 
 def product_list(list_product, df, list_api):
-	'''Return text with the product selected by the user '''
+	''' Return text with the product selected by the user
+
+		Inputs:
+		list_product(list of tuple[0](str)): Barcodes of the products
+		list_product(list of tuple[1](int)): Quantities associated with those products
+		df(pandas dataframe): Dataframe containing all products
+		list_api(tuple[0](str)): Name of the products to fill from the API
+		list_api(tuple[1](int)): Quantities associated with those products
+
+		Output:
+		text(str): Text listing the products entered by the user
+	'''
 	text = '<h2 style="color:#3C627E"> Selected Products</h2>'
 
 	if len(list_product) > 0:
