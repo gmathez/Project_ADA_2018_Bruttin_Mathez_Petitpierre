afa7e8941d3833ffb70582fca9f2578e0054c516
Rémi Petitpierre <38254995+RPetitpierre@users.noreply.github.com>
2018-12-15 08:58:12 +0100

    Comments added

--- product_better.py
+++ product_better.py
@@ -1,29 +1,53 @@
+# Imports
 import pandas as pd
 from collections import Counter
 
+
 def relevant_tag(dic, tags, threshold = 5):
-    """ Returns the least frequent tag from the list, who however has more than 'threshold' 
-    correspondancies in the database """
+    ''' Returns the least frequent tag from the list, who however has more than 'threshold' 
+        correspondancies in the database
+
+        Inputs:
+        dic(dictionary): contains the list of tags and the number of products associated to it
+        tags(list(string)): Contains all the tags of a product
+        threshold: The minimal number of neighbor with the same tag in order to consider it relevant
+            (default = 5)
+
+        Output:
+        sort(list): Sorted list of relevant tags (most relevant first)
+    '''
     
     # Init
     dict_, sort = {}, []
     
+    # Create a new dictionary dict_ which only contains the entries of dic
+    # that correspond to the product's tags
     for tag in tags:
         if tag in dic:
             dict_[tag] = dic[tag]
     
-    # Keep only tag which have more occurencies than threshold
+    # Keep only tags which have more occurencies than the threshold
     dict_ = {k: v for k, v in dict_.items() if v >= threshold}
+
     if len(dict_) > 0:
         # Returns the most relevant tags
         sort = sorted(dict_, key = dict_.get, reverse=False)
         return sort
+
     else :
         return None
 
+
 def list_df_tags(data_food):
-    """ List of all the tags found in the dataset. Count the number of occurences and rank them
-        according to this number. """
+    ''' List of all the tags found in the dataset. Count the number of occurences and rank them
+        according to this number
+
+        Input:
+        data_food(pandas dataframe): Dataset containing all products
+
+        Output:
+        (dictionary): List of all tags counted by number of occurences
+    '''
     
     # Split the tags found in the categories_tags columns
     all_tags = [tags.split(',') for tags in list(data_food[['categories_tags']].dropna().categories_tags)]
@@ -40,14 +64,28 @@ def list_df_tags(data_food):
     # Return the list by number of occurences 
     return dict(Counter(list_tags).most_common())
 
+
 def find_healthier_product(product_tuple, df, dic_tag):
-    ''' Replace the product by a product of the same category that has a better Nutriscore '''
+    ''' Replace the product by a product of the same category that has a better Nutriscore
+
+        Inputs:
+        product_tuple(tuple[0](str)): Barcode of the product to be eventually replaced
+        product_tuple(tuple[1](int)): Quantity of the product
+        df(pandas dataframe): Dataset containing all products
+        dic_tag(dictionary): List of all tags in the dataset counted by number of occurences
+
+        Outputs:
+        replaced(bool): The product was replaced by a healthier one (True/False)
+        best_product(pandas dataframe row):  Product to eventually replace the input product
+        best_sc(int): NutriScore of the best product
+        best_code(str): Barcode of the best product
+        old_sc(int): NutriScore of the input product
+        old_gr(str): From 'a' to 'e', NutriScore of the input product
+        new_gr(str): From 'a' to 'e', NutriScore of the best product
+    '''
     
-    # Initialize variables
+    # Initialize variable
     best_tag = ''
-    best_sc, best_product, best_code = 0, {}, ''
-    
-    # A boolean to check if the product has been replaced
     replaced = False
     
     # Find the product in the dataset
@@ -61,11 +99,13 @@ def find_healthier_product(product_tuple, df, dic_tag):
     old_gr = product['Predicted_NutriScore_grade'].upper()
     new_gr = product['Predicted_NutriScore_grade'].upper()
     
-    # Catch the best tag of the product
+    # Catch the input product's most relevant tag
     tags = product.categories_tags.split(',')
     best_tags = relevant_tag(dic_tag, tags, 10)
     if best_tags != None:
         best_tag = best_tags[0]
+
+    # Error handling : the product to replace has no reliable tag
     else:
         return False, best_product, best_sc, best_code, old_sc, old_gr, new_gr
     
@@ -87,7 +127,18 @@ def find_healthier_product(product_tuple, df, dic_tag):
        
     return replaced, best_product, best_sc, best_code, old_sc, old_gr, new_gr
 
+
 def Better_product_rec(list_product, df):
+    ''' Replace the product by a product of the same category that has a better Nutriscore
+
+        Inputs:
+        list_product(dictionary): List of the products entered by to user, to replace by healthier ones
+        df(pandas dataframe): Dataset containing all products
+
+        Output:
+        text(str): Text of the replacement suggestion
+    '''
+
     text = '<h2 style="color:#3C627E"> Healthier Product </h2>'
     dic_tag = list_df_tags(df)
 
